81ec7ebe4e9f4ee31bad4957e068aafff718fb4f
harsha1454 <saisriharshaprathipati@gmail.com>
2025-07-01 11:54:18 +0530

    data-structures.py

--- data-structures.py
+++ data-structures.py
@@ -15,3 +15,35 @@ print(arr)
 for i in arr:
     print(i,end=' ')
 print("\n 25 in array?",25 in arr)    
+text=input("enter a name")
+if text==text[::-1]:
+    print("palindrome")
+else:
+    print("not")
+freq={}
+for ch in text:
+    freq[ch]=freq.get(ch,0)+1
+print(freq)           
+
+'''arr of list of size n
+key for search element
+start with zero index
+compare arr[i]==key
+  arr[i]=key return index
+  else not(move to next index)
+repeat same steps till n-1  
+'''
+def linear_searh(arr,key):
+    for i in range(len(arr))
+size=int(input("enter the size of array:"))
+arr=[]
+print("enter the elements:")
+for i in range(size):
+    num=int(input(f"Element {i+1}:"))
+    arr.append(num)
+key= int(input("Enter the element to search")) 
+result=linear_search(arr,key)
+if result !=-1:
+    print(f"\n Element {key} found at {result} ")   
+else:
+    print(f"\n ")    
